57ea9868497ee8fbbaeb243193694aa3b4bff1b4
Thomas Fogarty <t.fogarty24@outlook.com>
2024-11-14 19:33:12 +0000

    improved login and register page styling

--- frontend/src/routes/login/LoginPage.jsx
+++ frontend/src/routes/login/LoginPage.jsx
@@ -2,6 +2,7 @@ import classes from "./LoginPage.module.css";
 import { Link } from "react-router-dom";
 import { useRef } from "react";
 import loginUser from "../../utility/authentication/login_user";
+import styles from './LoginPage.module.css'
 
 const LoginPage = () => {
 
@@ -20,21 +21,35 @@ const LoginPage = () => {
     }
 
   return (
-    <form onSubmit={loginHandler}>
-      <h1>Login</h1>
-      <div>
-        <label htmlFor="">Email</label>
-        <input type="email" name="email" ref={email}/>
-      </div>
-      <div>
-        <label htmlFor="">Password</label>
-        <input type="password" name="password" ref={password}/>
-      </div>
-      <button type="submit">Login</button>
-      <div>
-        <Link to={"/register"}>Or register an account here</Link>
-      </div>
-    </form>
+    <div className={styles.loginContainer}>
+      <form onSubmit={loginHandler} className={styles.loginForm}>
+        <h1 className={styles.loginTitle}>Login</h1>
+        <div className={styles.formGroup}>
+          <label htmlFor="email" className={styles.formLabel}>Email</label>
+          <input
+            type="email"
+            name="email"
+            ref={email}
+            required
+            className={styles.formInput}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="password" className={styles.formLabel}>Password</label>
+          <input
+            type="password"
+            name="password"
+            ref={password}
+            required
+            className={styles.formInput}
+          />
+        </div>
+        <button type="submit" className={styles.loginButton}>Login</button>
+        <div className={styles.registerLink}>
+          <Link to="/register">Or register an account here</Link>
+        </div>
+      </form>
+    </div>
   );
 };
 
--- frontend/src/routes/login/Register.jsx
+++ frontend/src/routes/login/Register.jsx
@@ -2,6 +2,7 @@ import classes from './LoginPage.module.css'
 import registerUser from '../../utility/authentication/register_user';
 import { useRef } from 'react';
 import { Link } from "react-router-dom";
+import styles from './RegisterPage.module.css';
 
 const Register = () => {
 
@@ -35,38 +36,66 @@ const Register = () => {
     }
 
   return (
-    <div>
-      <h1>Register</h1>
-      <form action="" onSubmit={registerUserHandler}>
-        <fieldset>
-          <label htmlFor="">Email</label>
-          <input type="email" name="email" id="email" ref={email}/>
-        </fieldset>
-        <fieldset>
-          <label htmlFor="">Password</label>
-          <input type="password" name="password" id="password" ref={password}/>
-        </fieldset>
-        <fieldset>
-          <label htmlFor="">First Name</label>
-          <input type="first_name" name="first_name" id="first_name" ref={firstName}/>
-        </fieldset>
-        <fieldset>
-          <label htmlFor="">Last Name</label>
-          <input type="last_name" name="last_name" id="last_name" ref={lastName}/>
-        </fieldset>
-        <fieldset>
-          <label htmlFor="">Account type</label>
-          <select name="account_type" id="account_type" >
-            <option value="admin">Admin</option>
-            <option value="user" selected>User</option>
-          </select>
-        </fieldset>
-        <button type='submit'>Submit</button>
-      </form>
-      <div>
-        <Link to={"/login"}>Go to Login</Link>
+    <div className={styles.registerContainer}>
+    <form onSubmit={registerUserHandler} className={styles.registerForm}>
+      <h1 className={styles.registerTitle}>Register</h1>
+      <fieldset className={styles.fieldset}>
+        <label htmlFor="email" className={styles.formLabel}>Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          ref={email}
+          className={styles.formInput}
+          required
+        />
+      </fieldset>
+      <fieldset className={styles.fieldset}>
+        <label htmlFor="password" className={styles.formLabel}>Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          ref={password}
+          className={styles.formInput}
+          required
+        />
+      </fieldset>
+      <fieldset className={styles.fieldset}>
+        <label htmlFor="first_name" className={styles.formLabel}>First Name</label>
+        <input
+          type="text"
+          name="first_name"
+          id="first_name"
+          ref={firstName}
+          className={styles.formInput}
+          required
+        />
+      </fieldset>
+      <fieldset className={styles.fieldset}>
+        <label htmlFor="last_name" className={styles.formLabel}>Last Name</label>
+        <input
+          type="text"
+          name="last_name"
+          id="last_name"
+          ref={lastName}
+          className={styles.formInput}
+          required
+        />
+      </fieldset>
+      <fieldset className={styles.fieldset}>
+        <label htmlFor="account_type" className={styles.formLabel}>Account Type</label>
+        <select name="account_type" id="account_type" className={styles.formSelect}>
+          <option value="admin">Admin</option>
+          <option value="user" selected>User</option>
+        </select>
+      </fieldset>
+      <button type="submit" className={styles.submitButton}>Submit</button>
+      <div className={styles.loginLink}>
+        <Link to="/login">Go to Login</Link>
       </div>
-    </div>
+    </form>
+  </div>
   );
 };
 
