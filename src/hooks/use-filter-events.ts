8ef856f5b06ff7578ebf5086d2f88b1ee246f7f8
Thomas Fogarty <t.fogarty24@outlook.com>
2025-06-21 13:13:40 +0100

    filter hook converted to TS and improved grouping logic added

---
+++ src/hooks/use-filter-events.ts
@@ -0,0 +1,93 @@
+// import { Event as EventType } from "../types/Events";
+// import { Filter as FilterType } from "../types/Filters";
+import { Event as EventType } from "../types/Events";
+import { Filter as FilterType } from "../types/filters";
+import { useMemo, useCallback } from "react";
+
+const useFilterEvents = (
+  events: EventType[],
+  filtersTypes: FilterType[]
+): {
+  filteredEvents: EventType[];
+  nextSevenDaysOfEvents: EventType[];
+  allUpcommingEvents: EventType[];
+  allPastEvents: EventType[];
+} => {
+
+  const filtersByType = useMemo(() => {
+    return Array.isArray(filtersTypes)
+      ? filtersTypes.reduce((acc, filter) => {
+          acc[filter.type] = acc[filter.type] || [];
+          acc[filter.type].push(filter.value);
+          return acc;
+        }, {} as Record<string, string[]>)
+      : {};
+  }, [filtersTypes]);
+
+  const filterByType = useCallback(
+    (events: EventType[]) => {
+      return events.filter(event => 
+        filtersByType.Type.includes(event.event_type)
+      );
+    },
+    [filtersByType]
+  );
+
+  // const filterByBooked = (events: EventType[]) => {
+
+  //   if (filtersByType.Booked.includes('notBooked')) {
+  //     return events.filter((event) => {
+  //       event.booked ===
+  //     })
+  //   }
+
+  //   return events.filter((event) => [
+  //     filtersByType.Booked.includes()
+  //   ])
+  // };
+
+  const filterNextSevenDays = (events: EventType[]) => {
+    const currentDate = new Date();
+
+    const futureDate = new Date();
+    futureDate.setDate(currentDate.getDate() + 7);
+
+    return events.filter((event) => {
+      const eventDate = new Date(event.event_date);
+      return eventDate >= currentDate && eventDate <= futureDate;
+    });
+  };
+
+  const filterAllUpcommingEvents = (events: EventType[]) => {
+    const currentDate = new Date();
+    return events.filter((event) => new Date(event.event_date) >= currentDate);
+  };
+
+  const filterAllPastEvents = (events: EventType[]) => {
+    const currentDate = new Date();
+    return events.filter((event) => new Date(event.event_date) < currentDate);
+  };
+
+  const filteredEvents = useMemo(() => {
+    let filtered = events;
+
+    if (filtersByType.Type && filtersByType.Type.length > 0) {
+      filtered = filterByType(filtered);
+    };
+
+    // if (filtersByType.Booked && filtersByType.Booked.length > 0) {
+    //   filtered = filterByBooked(filtered);
+    // }
+
+    return filtered;
+  }, [events, filtersByType, filterByType]);
+
+  return {
+    filteredEvents,
+    nextSevenDaysOfEvents: filterNextSevenDays(events),
+    allUpcommingEvents: filterAllUpcommingEvents(events),
+    allPastEvents: filterAllPastEvents(events),
+  };
+};
+
+export default useFilterEvents;
---
+++ src/types/Events.ts
@@ -0,0 +1,12 @@
+export interface Event {
+  created_at: string,
+  creator_user_id: number,
+  event_date: string,
+  event_description: string,
+  event_id: number,
+  event_img: string,
+  event_name: string,
+  event_time: string,
+  event_type: string
+};
+
